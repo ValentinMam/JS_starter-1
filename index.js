f89f8ab9481b62b531c99a1e20e24bced44635d7
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-02-20 13:55:26 +0100

    exercice 2 OK

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ affichageTotalLivres += totalLivres;
 affichageTotalLivres += " livres.";
 
 console.log(affichageTotalLivres);
+
+// Exercice 2
+
+const ticket = {
+  nomFilm: "Batman",
+  prix: 10,
+  numeroSalle: 3,
+};
+
+let nom = "David";
+
+// Bonjour nom, votre film NOMDEFILM est en salle NUMERODELASALE
+
+let texteAffichage =
+  "Bonjour " + nom + ", votre film " + ticket.nomFilm + " est en salle " + ticket.numeroSalle;
+
+console.log(texteAffichage);
