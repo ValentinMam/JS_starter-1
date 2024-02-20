29951aafba9d4dc12cb84b1012fb2ee0ec23d632
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-02-20 13:46:35 +0100

    exercice 1 OK

---
+++ index.js
@@ -0,0 +1,15 @@
+// Exercice 1
+
+let totalLivres = 500;
+
+totalLivres += 50;
+totalLivres -= 10;
+totalLivres += 5;
+
+console.log(totalLivres);
+
+let affichageTotalLivres = "Notre bibliothèque possède ";
+affichageTotalLivres += totalLivres;
+affichageTotalLivres += " livres.";
+
+console.log(affichageTotalLivres);
