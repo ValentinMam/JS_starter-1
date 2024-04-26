6a0a6c512c038dc7a20284671b1eda1fd33226d1
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-04-26 14:54:12 +0200

    CREATE

--- chapter_3/chapter_3.js
+++ chapter_3/chapter_3.js
@@ -27,12 +27,12 @@ for (let i = 0; i < listeInputRadio.length; i++) {
 // MODIFIER : UPDATE_____________________________________________________________________________
 //
 
-<img
-  id="premiereImage"
-  src="image.jpg"
-  alt="Ceci est une image de test"
-  class="photo flexCenter"
-/>;
+// <img
+//   id="premiereImage"
+//   src="image.jpg"
+//   alt="Ceci est une image de test"
+//   class="photo flexCenter"
+// />;
 
 // CHAPITRE 3 : setAttribute (permet de spécifier n’importe quel attribut)
 
@@ -55,3 +55,49 @@ baliseImage.setAttribute("alt", "Ceci est une image de test modifiée");
 baliseImage.src = "cheminImage.jpg";
 baliseImage.classList.add("nouvelleClasse");
 baliseImage.classList.remove("photo");
+
+//
+// CREER : CREATE_____________________________________________________________________________
+//
+
+// CHAPITRE 3 : createElement (créer une nouvelle balise)
+
+let nouvelElement = document.createElement("div");
+
+// Récupérer un élément parent existant
+let parentElement = document.getElementById("conteneur");
+// Ajouter le nouvel élément au parent
+parentElement.appendChild(nouvelElement);
+
+// Définition des variables contenant le texte du titre et du paragraphe
+let contenuTitre = "Azertype";
+let contenuParagraphe = "L'application pour apprendre à taper plus vite !";
+// Création d'un div avec createElement. Dans cette div, on va créer un titre h1 et un paragraphe p
+let nouvelleDiv = document.createElement("div");
+let nouveauTitre = document.createElement("h1");
+let nouveauParagraphe = document.createElement("p");
+// On ajoute du texte dans le titre et le paragraphe
+nouveauTitre.textContent = contenuTitre;
+nouveauParagraphe.textContent = contenuParagraphe;
+// On ajoute le titre et le paragraphe dans la div
+nouvelleDiv.appendChild(nouveauTitre);
+nouvelleDiv.appendChild(nouveauParagraphe);
+// On ajoute la div dans le body
+let body = document.querySelector("body");
+body.appendChild(nouvelleDiv);
+
+// CHAPITRE 3 : l’interpolation pour générer du HTML
+
+// let contenuTitre = "Azertype";
+// let contenuParagraphe = "L'application pour apprendre à taper plus vite !";
+// let div = `
+//     <div>
+//         <h1>${contenuTitre}</h1>
+//         <p>${contenuParagraphe}</p>
+//     </div>
+//     `;
+
+// CHAPITRE 3 : innerHTML (pour insérer du HTML)
+
+// let body = document.querySelector("body");
+body.innerHTML = div;
