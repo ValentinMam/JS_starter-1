9a6baeb7021b6ce78e75688f1fef71fbc279b370
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-04-26 16:42:04 +0200

    exercice 9 - 3 : affichez les mots que l’utilisateur doit recopier

--- chapter_3/scripts/script.js
+++ chapter_3/scripts/script.js
@@ -18,6 +18,11 @@ function afficherResultat(score, nbMotsProposes) {
   spanScore.innerText = affichageScore;
 }
 
+function afficherProposition(proposition) {
+  let zoneProposition = document.querySelector(".zoneProposition");
+  zoneProposition.innerText = proposition;
+}
+
 /**
  * Cette fonction lance le jeu.
  * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
@@ -27,12 +32,21 @@ function lancerJeu() {
 
   let score = 0;
   let nbMotsProposes = 0;
+  let i = 0;
 
   let btnValiderMot = document.getElementById("btnValiderMot");
   let inputEcriture = document.getElementById("inputEcriture");
-
+  afficherProposition(listeMots[i]);
   btnValiderMot.addEventListener("click", function () {
     console.log(inputEcriture.value);
+    i++;
+    inputEcriture.value = "";
+    if (listeMots[i] === undefined) {
+      afficherProposition("le jeu est fini !");
+      btnValiderMot.disabled = true;
+    } else {
+      afficherProposition(listeMots[i]);
+    }
   });
 
   afficherResultat(score, nbMotsProposes);
