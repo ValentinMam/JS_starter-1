ef85fa739b80c66968e14fa8fea3b753c3cd3966
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-04-26 06:50:16 +0200

    READ & UPDATE

--- chapter_3/chapter_3.js
+++ chapter_3/chapter_3.js
@@ -1,3 +1,7 @@
+//
+// RECUPERER : READ_____________________________________________________________________________
+//
+
 // CHAPITRE 3 : getElementById
 let baliseZoneProposition = document.getElementById("zoneProposition");
 console.log(baliseZoneProposition);
@@ -18,3 +22,36 @@ console.log(listeInputRadio);
 for (let i = 0; i < listeInputRadio.length; i++) {
   console.log(listeInputRadio[i]);
 }
+
+//
+// MODIFIER : UPDATE_____________________________________________________________________________
+//
+
+<img
+  id="premiereImage"
+  src="image.jpg"
+  alt="Ceci est une image de test"
+  class="photo flexCenter"
+/>;
+
+// CHAPITRE 3 : setAttribute (permet de spécifier n’importe quel attribut)
+
+let baliseImage = document.getElementById("premiereImage");
+baliseImage.setAttribute("alt", "Ceci est la nouvelle valeur de alt");
+
+// OU
+baliseImage.alt = "Ceci est une image de test modifiée";
+
+// CHAPITRE 3 : classList  (qui permet de modifier des classes)
+
+baliseImage.alt = "Ceci est une image de test modifiée";
+baliseImage.classList.add("nouvelleClasse");
+baliseImage.classList.remove("photo");
+
+// CHAPITRE 3 : vérification avec l'inspecteur
+
+let baliseImage = document.getElementById("premiereImage");
+baliseImage.setAttribute("alt", "Ceci est une image de test modifiée");
+baliseImage.src = "cheminImage.jpg";
+baliseImage.classList.add("nouvelleClasse");
+baliseImage.classList.remove("photo");
