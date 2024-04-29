ca12ab5ef8b327b911ea0de5d49bb1dac7326219
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-04-29 16:22:26 +0200

    exercice 10

--- chapter_4/scripts/script.js
+++ chapter_4/scripts/script.js
@@ -29,30 +29,47 @@ function afficherProposition(proposition) {
  */
 function lancerJeu() {
   // Initialisations
-
   let score = 0;
-
   let i = 0;
+  let listeProposition = listeMots;
 
   let btnValiderMot = document.getElementById("btnValiderMot");
   let inputEcriture = document.getElementById("inputEcriture");
-  afficherProposition(listeMots[i]);
-  btnValiderMot.addEventListener("click", function () {
-    console.log(inputEcriture.value);
-    if (inputEcriture.value === listeMots[i]) {
+
+  afficherProposition(listeProposition[i]);
+
+  // Gestion de l'événement click sur le bouton "valider"
+  btnValiderMot.addEventListener("click", () => {
+    if (inputEcriture.value === listeProposition[i]) {
       score++;
     }
     i++;
     afficherResultat(score, i);
-
     inputEcriture.value = "";
-    if (listeMots[i] === undefined) {
-      afficherProposition("le jeu est fini !");
+    if (listeProposition[i] === undefined) {
+      afficherProposition("Le jeu est fini");
       btnValiderMot.disabled = true;
     } else {
-      afficherProposition(listeMots[i]);
+      afficherProposition(listeProposition[i]);
     }
   });
+
+  // Gestion de l'événement change sur les boutons radios.
+  let listeBtnRadio = document.querySelectorAll(".optionSource input");
+  for (let index = 0; index < listeBtnRadio.length; index++) {
+    listeBtnRadio[index].addEventListener("change", (event) => {
+      // Si c'est le premier élément qui a été modifié, alors nous voulons
+      // jouer avec la listeMots.
+      if (event.target.value === "1") {
+        listeProposition = listeMots;
+      } else {
+        // Sinon nous voulons jouer avec la liste des phrases
+        listeProposition = listePhrases;
+      }
+      // Et on modifie l'affichage en direct.
+      afficherProposition(listeProposition[i]);
+    });
+  }
 
   afficherResultat(score, i);
 }
